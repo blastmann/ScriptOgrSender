effd366366e7c18337d4f19c62ca3b2582cb095d
Eddie Chan <blastmann@live.com>
2013-06-30 00:30:51 +0800

    Revert "refactored some code and add python3 support."
    This reverts commit 943895f4d4d48c06f7ea46f21756af7eedc66d0b.

--- scriptogrsender.py
+++ scriptogrsender.py
@@ -2,7 +2,8 @@ import sublime
 import sublime_plugin
 import json
 import os
-import urllib, urllib.request, urllib.parse, urllib.error
+import urllib
+import urllib2
 import threading
 
 # ScriptOgr.am app key
@@ -13,55 +14,55 @@ app_key = u'Zl61hxkhDb14f80dad2938ee022365a68e55d2098d'
 # Api Call
 class ScriptOgrApiCall(threading.Thread):
     """docstring for ScriptOgrApiCall"""
-    def __init__(self, action=None):
+    def __init__(self, filename, filedata, operation, user_id, proxy_server, timeout):
         threading.Thread.__init__(self)
-        if action is None:
-            self.action = {}
-        else:
-            self.action = action
-        self.response = None
-        self.result = None
+        self.filename  = filename
+        self.filedata  = filedata
+        self.operation = operation
+        self.timeout   = timeout
+        self.user_id   = user_id
+        self.proxy     = proxy_server
+        self.response  = None
+        self.result    = None
 
     def run(self):
         try:
-            opr = {'app_key': app_key, 'user_id': self.action['user_id']}
-            if self.action['operation'] == 'post':
-                opr['name'] = self.action['filename']
-                opr['text'] = self.action['content']
-            elif self.action['operation'] == 'delete':
-                opr['filename'] = self.action['filename']
+            opr = {'app_key': app_key, 'user_id': self.user_id}
+            if self.operation == 'post':
+                opr['name'] = self.filename
+                opr['text'] = self.filedata
+            elif self.operation == 'delete':
+                opr['filename'] = self.filename
 
-            self.post(opr)
+            self.post(self.operation, opr)
             self.parse_response()
             return
 
-        except urllib.error.HTTPError as e:
+        except (urllib2.HTTPError) as (e):
             err = '%s: HTTP error %s contacting API' % (__name__, str(e.code))
-        except urllib.error.URLError as e:
+        except (urllib2.URLError) as (e):
             err = '%s: URL error %s contacting API' % (__name__, str(e.code))
         sublime.error_message(err)
         self.result = False
 
     # Post our data
-    def post(self, data):
-        dataenc = urllib.parse.urlencode(data)
-        if self.action['proxy'] != '':
-            proxy = {'http': self.action['proxy']}
-            request = urllib.request.URLopener(proxy)
+    def post(self, method, data):
+        dataenc= urllib.urlencode(data)
+        if self.proxy != '':
+            proxy = urllib2.ProxyHandler(proxies = {'http' : self.proxy})
+            request = urllib2.build_opener(proxy)
         else:
-            request = urllib.request.URLopener()
-
-        print(dataenc)
-        http_file = request.open(base_url + self.action['operation'] + '/', dataenc)
+            request = urllib2.build_opener()
+        http_file = request.open(base_url + self.operation + '/', dataenc, timeout=self.timeout)
         self.response = http_file.read()
 
     # Parse the api response
     def parse_response(self):
         response = json.loads(self.response)
         if response['status'] == 'success':
-            if self.action['operation'] == 'post':
+            if self.operation == 'post':
                 self.response = 'Successfully post your article'
-            elif self.action['operation'] == 'delete':
+            elif self.operation == 'delete':
                 self.response = 'Successfully delete your article'
         elif response['status'] == 'failed':
             self.response = response['reason']
@@ -88,29 +89,30 @@ class ScriptOgrCommandBase(sublime_plugin.TextCommand):
         f, ext = os.path.splitext(basename)
         self.runCommand(edit, f)
 
-    def handle_threads(self, threads, i=0, indicator=0):
+    def handle_threads(self, threads, i=0, dir=0):
         next_threads = []
         for thread in threads:
             if thread.is_alive():
                 next_threads.append(thread)
             else:
-                print('\nScriptOgr.am api response: ' + thread.get_response() + '\n')
+                print '\nScriptOgr.am api response: ' + thread.get_response() + '\n'
                 sublime.status_message('ScriptOgr.am api response: ' + thread.get_response())
-            if not thread.result:
+            if thread.result == False:
                 continue
         threads = next_threads
 
         if len(threads):
             before = i % 8
-            after = 7 - before
+            after = (7) - before
             if not after:
-                indicator = -1
+                dir = -1
             if not before:
-                indicator = 1
-            i += indicator
-            self.view.set_status('operating', 'ScriptOgrSender is operating [%s=%s]' % (' ' * before, ' ' * after))
+                dir = 1
+            i += dir
+            self.view.set_status('operating', 'ScriptOgrSender is opearting [%s=%s]' % \
+                (' ' * before, ' ' * after))
 
-            sublime.set_timeout(lambda: self.handle_threads(threads, i, indicator), 100)
+            sublime.set_timeout(lambda: self.handle_threads(threads, i, dir), 100)
             return
         self.view.erase_status('operating')
 
@@ -126,15 +128,7 @@ class PostScrCommand(ScriptOgrCommandBase):
         threads = []
         for sel in sels:
             content = self.view.substr(sel).encode('utf8')
-            # data package
-            action = {
-                'filename': filename,
-                'content': content,
-                'operation': 'post',
-                'user_id': self.user_id,
-                'proxy': self.proxy_server
-            }
-            thread = ScriptOgrApiCall(action)
+            thread = ScriptOgrApiCall(filename, content, 'post', self.user_id, self.proxy_server, 500)
             threads.append(thread)
             thread.start()
 
@@ -149,14 +143,7 @@ class DelPostScrCommand(ScriptOgrCommandBase):
     def runCommand(self, edit, filename):
         # Start ScriptOgr.am api call in a thread
         threads = []
-        # data package
-        action = {
-            'filename': filename,
-            'operation': 'post',
-            'user_id': self.user_id,
-            'proxy': self.proxy_server
-        }
-        thread = ScriptOgrApiCall(action)
+        thread = ScriptOgrApiCall(filename, '', 'delete', self.user_id, self.proxy_server, 500)
         threads.append(thread)
         thread.start()
 
